6476b85791463d2bc39a21331467a47d95d2772d
abondar24 <abondar1992@gmail.com>
2017-02-14 22:55:24 -0800

    Advanced I/O: mapping,sort by inode,sort by block

---
+++ linux_sysprog/sort_block.h
@@ -0,0 +1,13 @@
+//
+// Created by abondar on 2/14/17.
+//
+
+#ifndef SOMEC_SORT_BLOCK_H
+#define SOMEC_SORT_BLOCK_H
+
+#endif //SOMEC_SORT_BLOCK_H
+
+
+int get_block(int fd, int logical_block);
+int get_nr_blocks(int fd);
+void print_blocks(int fd);
---
+++ linux_sysprog/sort_inode.h
@@ -0,0 +1,11 @@
+//
+// Created by abondar on 2/14/17.
+//
+
+#ifndef SOMEC_SORT_INODE_H
+#define SOMEC_SORT_INODE_H
+
+int get_inode(int fd);
+
+
+#endif //SOMEC_SORT_INODE_H
